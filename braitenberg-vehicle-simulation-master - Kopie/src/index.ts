77222d6bfc398fc0cc7454e3102fb45b1934f8b5
Vitalina <kasvv78@gmail.com>
2019-11-26 21:35:31 +0100

    deleted useless functions

--- braitenberg-vehicle-simulation-master - Kopie/src/index.ts
+++ braitenberg-vehicle-simulation-master - Kopie/src/index.ts
@@ -46,36 +46,6 @@ var mytestgame = new Phaser.Game(config);
 //Have a look at all the members of mytestgame in the console
 console.log(mytestgame);
 
-//create the callback function
-
-/*document.getElementById('testbutton').onclick = function() {
-  mytestgame.scene.scenes[1].createBarrier();
-};
-document.getElementById('blank').onclick = function() {
-  mytestgame.scene.scenes[1].createBlank();
-};
-document.getElementById('source').onclick = function() {
-  mytestgame.scene.scenes[1].createSource();
-};
-
-document.getElementById('prefab2a').onclick = function() {
-  mytestgame.scene.scenes[1].createPrefab2a();
-};
-document.getElementById('prefab2b').onclick = function() {
-  mytestgame.scene.scenes[1].createPrefab2b();
-};
-document.getElementById('prefab3a').onclick = function() {
-  mytestgame.scene.scenes[1].createPrefab3a();
-};
-document.getElementById('prefab3b').onclick = function() {
-  mytestgame.scene.scenes[1].createPrefab3b();
-};
-document.getElementById('prefab3b').onclick = function() {
-  mytestgame.scene.scenes[1].createPrefab3b();
-};*/
-
-
-
 document.getElementById('motorCanvas').onchange = function () {
     console.log("motor change");
 };  
